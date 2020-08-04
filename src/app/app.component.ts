2069188c2bb4a615a0ccbc4c7b2810f55caa22f0
Lucilef81 <lucile.franzinetti@gmail.com>
2020-08-04 19:17:50 +0200

    init app + fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-botdesign';
+export class AppComponent implements OnInit {
+  pokemons: any = [];
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPokemons();
+  }
+
+  fetchPokemons() {
+    this.http.get('../assets/pokemon.json')
+    .subscribe(success => {
+      this.pokemons = success;
+      console.log(this.pokemons);
+    })
+  }
+
 }
